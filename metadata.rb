a2f248162733afcd1ea9ed289b38901ceab65d08
Bill Parrott <bill@chimericdream.com>
2013-10-31 15:33:58 -0500

    adding berkshelf file and a bit more basic config options

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'quasarvm'
+maintainer       'Bill Parrott'
+maintainer_email 'bill@chimericdream.com'
+license          'MIT License'
+description      'Installs/Configures a Basic Web Development Server'
+version          '0.1.0'
+
+recipe "quasarvm", "Main Configuration"
