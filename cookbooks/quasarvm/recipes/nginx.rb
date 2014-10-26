8ae69218ede88d6eda84dc6a976bfcd00b2ff340
Bill Parrott <william.parrott@perceptivesoftware.com>
2014-10-26 01:02:44 -0500

    dynamic urls working so far; python & complex php apps w/ rewriting (symfony, wordpress, etc) still untested

--- cookbooks/quasarvm/recipes/nginx.rb
+++ cookbooks/quasarvm/recipes/nginx.rb
@@ -1,7 +1,7 @@
 # *.(local|vm).(dev|prd)
 nginx_site "local.dev" do
   name "local.dev"
-  host '~^(?<domain>[a-zA-Z0-9-]+)\.(local|vm)\.(?<devprd>dev|prd)$'
+  host '~^(?<domain>[a-zA-Z0-9][a-zA-Z0-9\.-]*)\.(local|vm)\.(?<devprd>dev|prd)$'
   root "${domain}_${devprd}/public_html"
   index "index.php index.html index.htm"
   location "try_files $uri $uri/ =404"
@@ -13,7 +13,7 @@ end
 # *.python.(dev|prd)
 nginx_site "python.dev" do
   name "python.dev"
-  host '~^(?<domain>[a-zA-Z0-9-]+)\.python\.(?<devprd>dev|prd)$'
+  host '~^(?<domain>[a-zA-Z0-9][a-zA-Z0-9\.-]*)\.python\.(?<devprd>dev|prd)$'
   root "${domain}_${devprd}/public"
   index "index.php index.html index.htm"
   location "try_files $uri $uri/ =404"
@@ -24,7 +24,7 @@ end
 # *.sf.(dev|prd)
 nginx_site "sf.dev" do
   name "sf.dev"
-  host '~^(?<domain>[a-zA-Z0-9-]+)\.sf\.(?<devprd>dev|prd)$'
+  host '~^(?<domain>[a-zA-Z0-9][a-zA-Z0-9\.-]*)\.sf\.(?<devprd>dev|prd)$'
   root "${domain}_${devprd}/web"
   index "index.php index.html index.htm"
   location "try_files $uri $uri/ =404"
