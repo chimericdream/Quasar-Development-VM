5f45466ad2eb1c9aaa50a2e457ef432d73913d74
Bill Parrott <william.parrott@perceptivesoftware.com>
2014-10-29 00:08:39 -0500

    add more php modules

--- cookbooks/quasarvm/recipes/default.rb
+++ cookbooks/quasarvm/recipes/default.rb
@@ -9,7 +9,7 @@ end
 
 include_recipe "php-fpm"
 
-%w{php5 php5-dev php5-mysql php5-curl php5-gd gawk}.each do |a_package|
+%w{php5 php5-curl php5-dev php5-gd php5-mcrypt php5-mysql php5-mysqlnd php5-odbc php5-pgsql php5-sqlite php5-sybase php5-tidy php5-xmlrpc php5-xsl gawk}.each do |a_package|
   package a_package
 end
 
