ea66e10360fb3149fcfb2aaf65c0b5cba9447a86
Bill Parrott <william.parrott@lexmark.com>
2016-04-25 19:14:43 -0500

    remove fpm from recipes

--- cookbooks/quasarvm/recipes/php.rb
+++ cookbooks/quasarvm/recipes/php.rb
@@ -3,10 +3,6 @@ apt_repository 'php' do
   distribution node['lsb']['codename']
 end
 
-include_recipe "php-fpm"
-
 %w{php7.0 php7.0-mysql}.each do |a_package|
   package a_package
 end
-
-php_fpm_pool "www"
