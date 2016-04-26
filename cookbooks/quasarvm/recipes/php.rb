191ab54016fb8ab298bba12819a754ceb9780413
Bill Parrott <william.parrott@lexmark.com>
2016-04-25 21:39:28 -0500

    make apache play nice with php7

--- cookbooks/quasarvm/recipes/php.rb
+++ cookbooks/quasarvm/recipes/php.rb
@@ -3,6 +3,6 @@ apt_repository 'php' do
   distribution node['lsb']['codename']
 end
 
-%w{php7.0 php7.0-mysql}.each do |a_package|
+%w{php7.0 php7.0-mysql libapache2-mod-php7.0}.each do |a_package|
   package a_package
 end
