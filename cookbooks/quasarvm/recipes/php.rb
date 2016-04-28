daee159bcfcf202573d9fdc96d2a12311e862a7f
Bill Parrott <william.parrott@lexmark.com>
2016-04-27 23:18:58 -0500

    add a bunch of php packages

--- cookbooks/quasarvm/recipes/php.rb
+++ cookbooks/quasarvm/recipes/php.rb
@@ -3,6 +3,15 @@ apt_repository 'php' do
   distribution node['lsb']['codename']
 end
 
-%w{php7.0 php7.0-mysql libapache2-mod-php7.0}.each do |a_package|
+%w{php7.0 php7.0-curl php7.0-dev php7.0-gd php7.0-mcrypt php7.0-mysql php7.0-mysqlnd php7.0-odbc php7.0-pgsql php7.0-sqlite php7.0-sybase php7.0-tidy php7.0-xmlrpc php7.0-xsl gawk libapache2-mod-php7.0}.each do |a_package|
   package a_package
+end
+
+%w{apache2 cli fpm}.each do |folder|
+  template "/etc/php/7.0/#{folder}/php.ini" do
+    source "php.ini.erb"
+    owner "root"
+    group "root"
+    mode 0644
+  end
 end
